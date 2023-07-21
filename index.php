4e9e2477184c7c8c4eb4fc60c4611a2819489707
Ezequiel Pereira Cardoso <130497986+Ezequiel2512@users.noreply.github.com>
2023-07-20 22:12:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="cadastra.css">
+</head>
+<body>
+
+    <header>
+        <h1>INSCREVA-SE</h1>
+    </header>
+    <main>
+        <button class="button"><a href="cadaluno.php">CADASTRAR</a></button>
+        <button class="button"><a href="listaalunos.php">LISTAR</a></button>
+    </main>
+   
+  <footer>
+      <h4>@if baiano</h4>
+  </footer>
+
+</body>
+</html>
